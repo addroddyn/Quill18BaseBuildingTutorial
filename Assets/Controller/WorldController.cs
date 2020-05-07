26c1911977870f15b21aa89d50fe6d6b2598fb5c
Istvan Tamas <tamas.s.istvan@gmail.com>
2020-05-07 20:07:17 +0300

    Hopefully this gitignore works

---
+++ Assets/Controller/WorldController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldController : MonoBehaviour
+{
+    World _world;
+
+    [SerializeField]
+    private Sprite floorFilledSprite;
+
+
+    void Start()
+    {
+        _world = new World();
+
+        // Create a GameObject for each tile
+        for (int x = 0; x < _world.Width; x++)
+        {
+            for (int y = 0; y < _world.Height; y++)
+            {
+                Tile _tileWorld = _world.GetTileAt(x, y);
+                GameObject _tileGameObject = new GameObject();
+                _tileGameObject.name = "Tile_" + x + "_" + y;
+                _tileGameObject.transform.position = new Vector3(_tileWorld.XPosition, _tileWorld.YPosition, 0);
+                _tileGameObject.transform.SetParent(this.transform);
+                _tileGameObject.AddComponent<SpriteRenderer>();
+
+                // we set the Tile object's '_tileTypeChangedCallback' Action to this method
+                _tileWorld.RegisterTileTypeChangeCallback((tile) => { OnTileTypeChanged(tile, _tileGameObject); });
+            }
+        }
+
+        _world.RandomizeTiles();
+    }
+
+    private void Update()
+    {
+        
+    }
+
+
+    void OnTileTypeChanged(Tile tileWorld, GameObject tileGameObject)
+    {
+        if (tileWorld.Type == Tile.TileType.Floor)
+        {
+            tileGameObject.GetComponent<SpriteRenderer>().sprite = floorFilledSprite;
+        }
+        else if (tileWorld.Type == Tile.TileType.Empty)
+        {
+            tileGameObject.GetComponent<SpriteRenderer>().sprite = null;
+        }
+        else
+        {
+            Debug.LogError("OnTileTypeChanged - Unrecognized tile type");
+        }
+    }
+
+}
---
+++ Assets/Model/Tile.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile
+{
+
+    #region Tile Type stuff
+    public enum TileType { Empty, Floor};
+
+    TileType _type = TileType.Empty;
+
+    Action<Tile> _tileTypeChangedCallback;
+
+    public TileType Type
+    {
+        get => _type;
+        set
+        {
+            TileType oldType = _type;
+            _type = value;
+            if (_tileTypeChangedCallback != null && oldType != _type)
+            {
+                _tileTypeChangedCallback(this);
+            }
+        }
+    }
+
+    public void RegisterTileTypeChangeCallback(Action<Tile> callback)
+    {
+        _tileTypeChangedCallback += callback;
+    }
+    public void UnRegisterTileTypeChangeCallback(Action<Tile> callback)
+    {
+        _tileTypeChangedCallback -= callback;
+    }
+
+    #endregion
+
+    LooseObject looseObject;
+    InstalledObject installedObject;
+
+    World _world;
+    int _xPosition;
+    int _yPosition;
+
+    public int XPosition => _xPosition;
+    public int YPosition => _yPosition;
+
+    public Tile(World world, int x, int y)
+    {
+        this._world = world;
+        this._xPosition = x;
+        this._yPosition = y;
+    }
+
+
+}
